f6e781d7c5f775fdaef585f0b67d7fed0305d376
Matt Cao <piggy925@163.com>
2019-07-07 19:03:28 +0800

    Fixed issue about storing date time.

--- app/src/main/java/com/mattcao/androidlearningproject/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/mattcao/androidlearningproject/database/CrimeCursorWrapper.java
@@ -18,7 +18,7 @@ public class CrimeCursorWrapper extends CursorWrapper {
     public Crime getCrime() {
         String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
         String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
-        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        String date = getString(getColumnIndex(CrimeTable.Cols.DATE));
         int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
         String suspect = getString(getColumnIndex(CrimeTable.Cols.SUSPECT));
 
